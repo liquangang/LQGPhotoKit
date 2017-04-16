994e4a6d6c51d0d4d5508c4fc8877e739718f605
liquangang <sgliquangang@163.com>
2017-04-16 20:05:45 +0800

    Initial Commit

---
+++ LQGPhotoKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LQGPhotoKit
+//
+//  Created by liquangang on 2017/4/16.
+//  Copyright © 2017年 liquangang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
